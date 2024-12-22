6a74584240cbb82a4a6c1c4d3b564d96b8677058
Guilherme <guilhermepasan@gmail.com>
2024-12-22 14:44:32 -0300

    Edit and delete.

--- index.js
+++ index.js
@@ -18,16 +18,47 @@ app.get('/new', (req, res) => {
     res.render('new-post.ejs');
 });
 
+app.get('/posts/:id', (req, res) => {
+    const post = posts[req.params.id];
+    if (post) {
+        res.render('post', { post, id: req.params.id }); // Pass the id to the view
+    } else {
+        res.status(404).send('Post not found');
+    }
+});
+
+app.get('/posts/:id/edit', (req, res) => {
+    const post = posts[req.params.id];
+    if (post) {
+        res.render('edit-post', { post, id: req.params.id });
+    } else {
+        res.status(404).send('Post not found');
+    }
+});
+
 app.post('/new', (req, res) => {
     const { title, content } = req.body;
     posts.push({ title, content });
     res.redirect('/');
 });
 
-app.get('/posts/:id', (req, res) => {
+app.post('/posts/:id/edit', (req, res) => {
     const post = posts[req.params.id];
     if (post) {
-        res.render('post', { post });
+        const { title, content } = req.body;
+        post.title = title;
+        post.content = content;
+        res.redirect(`/posts/${req.params.id}`);
+    } else {
+        res.status(404).send('Post not found');
+    }
+});
+
+app.post('/posts/:id/delete', (req, res) => {
+    const postIndex = req.params.id;
+    if (posts[postIndex]) {
+        posts.splice(postIndex, 1);
+        res.redirect('/');
     } else {
         res.status(404).send('Post not found');
     }
